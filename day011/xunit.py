39f327fc454c1f75a6ff68badd4721b8b0175299
akiyoshi-huji <akiyoshihuji@gmail.com>
2020-08-09 18:32:40 +0900

    day011 xunit.py p.164

--- day011/xunit.py
+++ day011/xunit.py
@@ -10,6 +10,7 @@ class TestCase:
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return TestResult()
 
 class WasRun(TestCase):
     def setUp(self):
@@ -19,10 +20,21 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + "tearDown "
 
+class TestResult:
+    def __init__(self):
+        self.runCount = 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
