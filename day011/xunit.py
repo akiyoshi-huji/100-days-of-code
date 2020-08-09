fb843e5647332def647cf5e6c0a17d88919f2412
akiyoshi-huji <akiyoshihuji@gmail.com>
2020-08-09 17:44:55 +0900

    day011 xunit.py p.159

--- day011/xunit.py
+++ day011/xunit.py
@@ -9,7 +9,7 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
-        self.tearDown
+        self.tearDown()
 
 class WasRun(TestCase):
     def setUp(self):
