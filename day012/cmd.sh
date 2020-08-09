0f2264c5d63bfdcc36cb1ac5ef464748b18bffd4
akiyoshi-huji <akiyoshihuji@gmail.com>
2020-08-10 02:30:36 +0900

    day012 docker mysql container and DockerVolume

--- day012/cmd.sh
+++ day012/cmd.sh
@@ -3,3 +3,54 @@ docker image build -t example/mysql-data:latest .
 
 # run container
 docker container run -d --name mysql-data example/mysql-data:latest
+
+# Data Volume container mount on MYSQL container
+docker container run -d --rm --name mysql \
+-e "MYSQL_ALLOW_EMPTY_PASSWORD=yes" \
+-e "MYSQL_DATABASE=volume_test" \
+-e "MYSQL_USER=example" \
+-e "MYSQL_PASSWORD=example" \
+--volumes-from mysql-data \
+mysql:5.7
+
+# login mysql container
+docker container exec -it mysql mysql -u root -p volume_test
+
+# CREATE TABLE on mysql
+#>mysql
+CREATE TABLE user(
+  id int PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+INSERT INTO user (name) VALUES ('gihyo'), ('docker'), ('Solomon Hykes');
+
+# result
+#mysql> select * from user;
+#+----+---------------+
+#| id | name          |
+#+----+---------------+
+#|  1 | gihyo         |
+#|  2 | docker        |
+#|  3 | Solomon Hykes |
+#+----+---------------+
+#3 rows in set (0.00 sec)
+
+# stop mysql container
+docker container stop mysql
+
+# check user TABLE
+docker container run -d --rm --name mysql \
+-e "MYSQL_ALLOW_EMPTY_PASSWORD=yes" \
+-e "MYSQL_DATABASE=volume_test" \
+-e "MYSQL_USER=example" \
+-e "MYSQL_PASSWORD=example" \
+--volumes-from mysql-data \
+mysql:5.7
+
+# login
+docker container exec -it mysql mysql -u root -p volume_test
+
+# select table
+#mysql>
+select * from user;
