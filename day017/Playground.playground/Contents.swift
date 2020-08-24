73fa623dea761cf2bcd206c67e1b1ecfc6a973ec
akiyoshi-huji <akiyoshihuji@gmail.com>
2020-08-24 16:17:59 +0900

    switch

--- day017/Playground.playground/Contents.swift
+++ day017/Playground.playground/Contents.swift
@@ -1,4 +1,16 @@
-var price = 10000
-var tax = 0.10
-var res = Double(price) * Double(tax)
-print(res)
+var month = 1
+
+switch month {
+case 1...2:
+    print("winter")
+case 3...5:
+    print("spring")
+case 6...8:
+    print("summer")
+case 9...11:
+    print("autumn")
+case 12:
+    print("siwasu")
+default:
+    print("error")
+}
