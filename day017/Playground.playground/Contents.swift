610ff39f37554f49b69941c450316beee35ebc87
akiyoshi-huji <akiyoshihuji@gmail.com>
2020-08-24 16:42:36 +0900

    func

--- day017/Playground.playground/Contents.swift
+++ day017/Playground.playground/Contents.swift
@@ -1,16 +1,7 @@
-var month = 1
-
-switch month {
-case 1...2:
-    print("winter")
-case 3...5:
-    print("spring")
-case 6...8:
-    print("summer")
-case 9...11:
-    print("autumn")
-case 12:
-    print("siwasu")
-default:
-    print("error")
+func data(price:Double)->Void {
+    print(price)
 }
+
+data(price: 100)
+data(price: 100.00)
+data(price: 100.001)
